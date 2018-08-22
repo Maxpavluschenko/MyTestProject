09873a1e89859e91702767578d709e01d1ff401d
Maksym Pavlushchenko <max.pavluschenko@gmail.com>
2018-08-22 08:54:47 +0000

    created a list of active users

--- app/js/index.js
+++ app/js/index.js
@@ -1,26 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("http://www.json-generator.com/api/json/get/cpTmnSrPCa?indent=2")
-    .then(response => response.json())
-    .then(dataArray => {
-      if (dataArray == undefined || dataArray == null || dataArray == [])
-        return;
+    fetch("http://www.json-generator.com/api/json/get/cpTmnSrPCa?indent=2")
+        .then(response => response.json())
+        .then(dataArray => {
+            if (dataArray == undefined || dataArray == null || dataArray == [])
+                return;
+            
+            let dataArraySliced = dataArray.slice(0, 15);
+            let container = document.getElementsByClassName("container");
+            
+            addEventListenerOnButton(
+                () => filterActiveCards(dataArraySliced, container[0])
+            );
 
-      let container = document.getElementsByClassName("container");
-
-      for (const index of dataArray.slice(0, 15).keys()) {
-        container[0].appendChild(
-          getTemlate(dataArray[index].name, dataArray[index].picture, dataArray[index].about, dataArray[index].email, dataArray[index].isActive)
-        );
-      }
-    });
+            for (const index of dataArraySliced.keys()) {
+                container[0].appendChild(
+                    getTemlate(dataArray[index].name, 
+                               dataArray[index].picture, 
+                               dataArray[index].about, 
+                               dataArray[index].email, 
+                               dataArray[index].isActive)
+                );
+            }
+        });
 });
 
 function getTemlate(name, image, about, email, isActive) {
-  const col = document.createElement("div");
+    const cardElement = document.createElement("div");
 
-  col.innerHTML =
-
-    `<div class="card">
+    cardElement.innerHTML =
+        `<div class="card">
                 <div class="card-header">
                     <img src="${image}" alt="avatar">
                     <p>${name}</p>
@@ -36,11 +44,45 @@ function getTemlate(name, image, about, email, isActive) {
                     <div class="mail">
                         <a href="#">${email}</a>
                     </div>
-                    <div class="Active"></div>
+                    <div class="Active">${isActive}</div>
                 </div>
             </div>`;
 
-  col.classList.add("col-sm-6");
+    cardElement.classList.add("col-sm-6");
 
-  return col;
+    return cardElement;
 }
+
+function addEventListenerOnButton(handler) {
+    let filterButton = document.getElementById('filter-button'); 
+    filterButton.addEventListener('click', handler);
+}
+
+function filterActiveCards(dataArray, cntnr) {     
+        cntnr.innerHTML = "";
+        
+        for (const index of dataArray.keys()) {
+            if (dataArray[index].isActive) {
+                cntnr.appendChild(
+                    getTemlate(dataArray[index].name, 
+                               dataArray[index].picture, 
+                               dataArray[index].about, 
+                               dataArray[index].email, 
+                               dataArray[index].isActive)
+                );
+            }
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
